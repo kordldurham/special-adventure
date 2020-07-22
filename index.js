59f07a988a64cf2d992716b9c01deeb7a1edfb46
kordldurham <66217662+kordldurham@users.noreply.github.com>
2020-07-21 20:34:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const pets = [
     { name: `Rorschach`, type: `Dog`, breed: `Catahoula Leopard Hound`, age: 1, photo: `https://dogsaholic.com/wp-content/uploads/2016/06/Catahoula-Leopard-Dogs.jpg` },
     { name: `Ol' One Eye`, type: `Fish`, breed: `Diamond Tetra`, age: `?`, photo: `https://cdn.shopify.com/s/files/1/1074/0538/products/tetra-diamond-tetra-1_800x.jpg?v=1526683166` },
     { name: `The Lone Wanderer`, type: `Shrimp`, breed: `Ghost Shrimp`, age: `?`, photo: `https://petsoid.com/wp-content/uploads/2020/01/ghost-shrimp-black-background.jpg` },
-    { name: `Momma`, type: `Fish`, breed: `White Molly`, age: `?`, photo: `https://dlgdxii3fgupk.cloudfront.net/myaquariumclub.com/images/fbfiles/images/IMG_1601_v_1484231414.PNG` }
-
+    { name: `Momma`, type: `Fish`, breed: `White Molly`, age: `?`, photo: `https://dlgdxii3fgupk.cloudfront.net/myaquariumclub.com/images/fbfiles/images/IMG_1601_v_1484231414.PNG` },
+    { name: `Notorious F.I.S.H`, breed: `Black Molly`, age: `?`, photo: `https://i.ytimg.com/vi/pZ1JDOzYCdE/hqdefault.jpg` },
+    { name: `Nu-Fish`, breed: `Black-White Molly?`, age: `2 weeks`, photo: `https://wikiclipart.com/wp-content/uploads/2016/10/Printable-question-mark-clipart.png` }
 ];
