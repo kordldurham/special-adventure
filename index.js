5c0b616cd178f43eb2d8f06fe681083ed37bf609
kordldurham <66217662+kordldurham@users.noreply.github.com>
2020-07-21 20:52:40 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const pets = [
-    { name: `Bocephus`, type: `Dog`, breed: `Short Haired Dapple Dauschund`, age: 10 + ` years`, photo: `https://thisis.dog/wp-content/uploads/2019/11/IMG_20190127_163242-1068x801.jpg` },
+    { name: `Lacey`, type: `Dog`, breed: `Beagle Mix`, age: 14 + ` years(deceased)`, photo: `https://www.chicagotribune.com/resizer/vZ43fxKOqeXvOHaf2BtTATZ0dTI=/fit-in/800x533/smart/filters:fill(black)/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/N4MRFCDXY5GPBALZTTYJL6MPF4.jpg` },
+    { name: `Bocephus`, type: `Dog`, breed: `Short Haired Dapple Dauschund`, age: 10 + ` years(deceased)`, photo: `https://thisis.dog/wp-content/uploads/2019/11/IMG_20190127_163242-1068x801.jpg` },
     { name: `Rorschach`, type: `Dog`, breed: `Catahoula Leopard Hound`, age: `> 1 year`, photo: `https://i.imgur.com/OZKy3u5.jpeg` },
     { name: `Ol' One Eye`, type: `Fish`, breed: `Diamond Tetra`, age: `?`, photo: `https://cdn.shopify.com/s/files/1/1074/0538/products/tetra-diamond-tetra-1_800x.jpg?v=1526683166` },
     { name: `The Lone Wanderer`, type: `Shrimp`, breed: `Ghost Shrimp`, age: `?`, photo: `https://petsoid.com/wp-content/uploads/2020/01/ghost-shrimp-black-background.jpg` },
