9eba4f6ef028ed47d83863cc062c6a071680966b
kordldurham <66217662+kordldurham@users.noreply.github.com>
2020-07-21 21:08:29 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const pets = [
     { name: `Lacey`, type: `Dog`, breed: `Beagle Mix`, age: 14 + ` years(deceased)`, photo: `https://www.chicagotribune.com/resizer/vZ43fxKOqeXvOHaf2BtTATZ0dTI=/fit-in/800x533/smart/filters:fill(black)/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/N4MRFCDXY5GPBALZTTYJL6MPF4.jpg` },
     { name: `Bocephus`, type: `Dog`, breed: `Short Haired Dapple Dauschund`, age: 10 + ` years(deceased)`, photo: `https://thisis.dog/wp-content/uploads/2019/11/IMG_20190127_163242-1068x801.jpg` },
     { name: `Rorschach`, type: `Dog`, breed: `Catahoula Leopard Hound`, age: `> 1 year`, photo: `https://i.imgur.com/OZKy3u5.jpeg` },
-    { name: `Ol' One Eye`, type: `Fish`, breed: `Diamond Tetra`, age: `?`, photo: `https://cdn.shopify.com/s/files/1/1074/0538/products/tetra-diamond-tetra-1_800x.jpg?v=1526683166` },
-    { name: `The Lone Wanderer`, type: `Shrimp`, breed: `Ghost Shrimp`, age: `?`, photo: `https://petsoid.com/wp-content/uploads/2020/01/ghost-shrimp-black-background.jpg` },
-    { name: `Momma`, type: `Fish`, breed: `White Molly`, age: `?`, photo: `https://dlgdxii3fgupk.cloudfront.net/myaquariumclub.com/images/fbfiles/images/IMG_1601_v_1484231414.PNG` },
-    { name: `Notorious F.I.S.H`, breed: `Black Molly`, age: `?`, photo: `https://i.ytimg.com/vi/pZ1JDOzYCdE/hqdefault.jpg` },
-    { name: `Nu-Fish`, breed: `Black-White Molly?`, age: `2 weeks`, photo: `https://wikiclipart.com/wp-content/uploads/2016/10/Printable-question-mark-clipart.png` }
+    { name: `Ol' One Eye`, type: `Fish`, breed: `Diamond Tetra`, age: `?`, photo: `https://i.imgur.com/U3KD5w3.jpg` },
+    { name: `The Lone Wanderer`, type: `Shrimp`, breed: `Ghost Shrimp`, age: `?`, photo: `https://i.imgur.com/AG7JZGq.jpg` },
+    { name: `(unnamed)`, type: `Shrimp`, breed: `Ghost Shrimp`, age: `1 week`, photo: `https://i.imgur.com/drfZsNy.jpg` },
+    { name: `Momma`, type: `Fish`, breed: `White Molly`, age: `?`, photo: `https://i.imgur.com/Cs0Ppc5.jpg` },
+    { name: `Notorious F.I.S.H`, type: `Fish`, breed: `Black Molly`, age: `?`, photo: `https://i.imgur.com/fgo5kun.jpg` },
+    { name: `Nu-Fish`, type: `Fish`, breed: `Black-White Molly?`, age: `1 week`, photo: `https://i.imgur.com/OobkWjV.jpg` }
 ];
